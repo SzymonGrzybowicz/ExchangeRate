88eae3748d17b2060e7a2298830c67d0b24d4e76
szymon.grzybowicz <szymon.grzybowicz@gmail.com>
2020-09-03 13:19:51 +0200

    add JsonRateConverter tests

--- src/main/java/exchange_rate/nbp_api_client/converter/RateConverter.java
+++ src/main/java/exchange_rate/nbp_api_client/converter/RateConverter.java
@@ -1,7 +1,12 @@
 package exchange_rate.nbp_api_client.converter;
 
+import com.sun.istack.Nullable;
+
 import exchange_rate.nbp_api_client.dto.ExchangeRate;
 
 public interface RateConverter {
+
+	@Nullable
 	ExchangeRate convertResponse(String response);
+
 }
---
+++ src/test/java/exchange_rate/nbp_api_client/converter/JsonRateConverterTestSuite.java
@@ -0,0 +1,95 @@
+package exchange_rate.nbp_api_client.converter;
+
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import exchange_rate.nbp_api_client.Currency;
+import exchange_rate.nbp_api_client.converter.json.JsonRateConverter;
+import exchange_rate.nbp_api_client.dto.ExchangeRate;
+
+public class JsonRateConverterTestSuite {
+
+	private final String CORRECT_RESPONSE = "{\"table\":\"A\",\"currency\":\"euro\",\"code\":\"EUR\",\"rates\":[{\"no\":\"172/A/NBP/2020\",\"effectiveDate\":\"2020-09-03\",\"mid\":4.4181}]}";
+	private final String WRONG_CURRENCY_CODE_RESPONSE = "{\"table\":\"A\",\"currency\":\"euro\",\"code\":\"STH\",\"rates\":[{\"no\":\"172/A/NBP/2020\",\"effectiveDate\":\"2020-09-03\",\"mid\":4.4181}]}";
+	private final String NO_RATE_RESPONSE = "{\"table\":\"A\",\"currency\":\"euro\",\"code\":\"EUR\"}";
+	private final String EMPTY_RATE_RESPONSE = "{\"table\":\"A\",\"currency\":\"euro\",\"code\":\"EUR\",\"rates\":[]}";
+
+	@Test
+	public void test_convert_correct_response() {
+		// Given
+		RateConverter converter = new JsonRateConverter();
+
+		// When
+		ExchangeRate exchangeRate = converter.convertResponse(CORRECT_RESPONSE);
+
+		// Then
+		Assert.assertEquals(Currency.EURO, exchangeRate.getCurrency());
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		Assert.assertEquals("2020-09-03", df.format(exchangeRate.getDate()));
+		Assert.assertEquals(new BigDecimal("4.4181"), exchangeRate.getRate());
+	}
+
+	@Test
+	public void test_convert_wrong_currency_code() {
+		// Given
+		RateConverter converter = new JsonRateConverter();
+
+		// When
+		ExchangeRate exchangeRate = converter.convertResponse(WRONG_CURRENCY_CODE_RESPONSE);
+
+		// Then
+		Assert.assertEquals(null, exchangeRate);
+	}
+
+	@Test
+	public void test_convert_no_rate_response() {
+		// Given
+		RateConverter converter = new JsonRateConverter();
+
+		// When
+		ExchangeRate exchangeRate = converter.convertResponse(NO_RATE_RESPONSE);
+
+		// Then
+		Assert.assertEquals(null, exchangeRate);
+	}
+
+	@Test
+	public void test_convert_empty_rate_response() {
+		// Given
+		RateConverter converter = new JsonRateConverter();
+
+		// When
+		ExchangeRate exchangeRate = converter.convertResponse(EMPTY_RATE_RESPONSE);
+
+		// Then
+		Assert.assertEquals(null, exchangeRate);
+	}
+
+	@Test
+	public void test_convert_empty_string() {
+		// Given
+		RateConverter converter = new JsonRateConverter();
+
+		// When
+		ExchangeRate exchangeRate = converter.convertResponse("");
+
+		// Then
+		Assert.assertEquals(null, exchangeRate);
+	}
+
+	@Test
+	public void test_convert_wrong_string() {
+		// Given
+		RateConverter converter = new JsonRateConverter();
+
+		// When
+		ExchangeRate exchangeRate = converter.convertResponse("testWRONGstring");
+
+		// Then
+		Assert.assertEquals(null, exchangeRate);
+
+	}
+}
